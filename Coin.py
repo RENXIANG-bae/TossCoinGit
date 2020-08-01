1df707e9ac0559a781d5d6d3ccf1d0a72037214d
柏本仁香 <kimika@kashiwahonnikanoMacBook-Pro.local>
2020-08-01 23:08:49 +0900

    HoT:Initial revision

---
+++ Coin.py
@@ -0,0 +1,17 @@
+import random
+
+count1=0
+count2=0
+A=["Heads","Tails"]
+
+print("Tossing a coin...")
+
+for i in range(1,4):
+ wasi=random.choice(A)
+ print("Round %d: %s" %(i,wasi))
+ if wasi=="Heads":
+     count1+=1
+ else:
+     count2+=1
+
+print("Heads:%d Tails:%d" %(count1,count2))
